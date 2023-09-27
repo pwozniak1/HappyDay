e0845a3a44eb93ab8a9bcf7995ef6bc9f2c74fff
Piotr Woźniak <piotr.wozniak.career@gmail.com>
2023-09-27 16:03:18 +0200

    Initial Commit

---
+++ HappyDay/HappyDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyDayApp.swift
+//  HappyDay
+//
+//  Created by Piotr Woźniak on 27/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct HappyDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
